bf75fd5f56f822741897af8b1a83b9ae19a5340a
NadavS <100477080+NadavS5@users.noreply.github.com>
2024-06-04 22:10:25 +0300

    Add files via upload

---
+++ bigmaze.py
@@ -0,0 +1,148 @@
+import os, time, queue, sys
+#from colorama import Fore
+from PIL import Image#, ImageDraw    
+import colorsys
+
+
+startTime = time.time()
+with Image.open(sys.argv[1]) as im:
+    width, height = im.size
+    px = im.load()
+
+
+PIXEL_SIZE = 1
+
+
+start = (width-1,0)
+end = (1,height-1)
+
+
+queue = queue.Queue()
+
+
+done = []
+
+
+
+directions = {(PIXEL_SIZE,0),(0,PIXEL_SIZE),(0,-PIXEL_SIZE),(-PIXEL_SIZE,0)}
+
+def isValid(current):
+
+    y = current[0]
+    x = current[1] 
+    
+    if(y>= height):
+        return False
+    
+    if(x>= width):
+        return False
+    
+    if(x <0 or y <0) :
+        return False
+
+
+    #if(px[current] == (0,0,0)):
+    #   print(str(current) + str(px[current])) 
+
+    # in yossi's orginal picture the touple length for the colors is 4 and phtoshop' s is 3 
+    #so i re-exported it in photoshop 
+    
+    return px[current] != (0,0,0)
+    
+    
+    return False
+
+def getNext(current , dirr):
+    
+    return (current[0] + dirr[0], current[1] + dirr[1])
+
+win = False
+def isWin(current):
+    y = current[0] 
+    x = current[1] 
+
+    
+    #if (px[current] == (0,255,0)):
+      #  
+       # return True
+
+    return current == end
+
+    return False
+
+
+
+
+
+
+
+
+queue.put(start)
+current = start
+
+bfsPaths = {} 
+
+
+
+
+
+while (not queue.empty()and not win):
+    current = queue.get(0)
+    
+
+    if(isWin(current)):
+        total = round(time.time() - startTime, 3)
+       
+        print("solved in: " + str(total) + " seconds")
+        end = current
+        win = True
+        break
+   
+    #array[current[0]][current[1]] = 3 #show what exploreds
+    
+
+    #array[current[0]][current[1]] = 3
+    
+   
+
+    for dirr in directions: 
+        next = getNext(current, dirr)
+        if(isValid(next) and next not in done):
+            done.append(next)
+            queue.put(next)
+            bfsPaths[next] = current 
+ 
+solution = im.copy()
+
+
+
+
+cell = end
+def rgb_to_int_tuple(rgb):
+    """Convert RGB values from floats (0-1) to integers (0-255)."""
+    return tuple(int(x * 255) for x in rgb)
+
+
+
+green = 0
+
+hue = 0
+
+if(win):
+    
+    while cell != start:
+        
+        
+        #print(cell)
+        #img1 = ImageDraw.Draw(solution)   
+        
+        hue += 0.0005
+        rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+        
+
+        solution.putpixel(cell, rgb_to_int_tuple(rgb)) 
+        cell = bfsPaths[cell]
+
+
+solution.save("mazesolution.png")
+solution.show()
